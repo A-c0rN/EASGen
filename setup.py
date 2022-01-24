eccdb18006a6382fac75ad72e7c9d042f4092ac1
A-c0rN <mayer.brigan@gmail.com>
2022-01-23 22:40:50 -0600

    Update everything

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='EASGen',
     packages=find_packages(include=['EASGen']),
-    version='0.1.0',
+    version='0.1.1',
     description='A Python library to generate EAS SAME Audio using Raw Data',
     author='A-c0rN',
     license='ODbL-1.0',
