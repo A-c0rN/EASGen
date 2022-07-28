8b767ee34b4628a3bdd41448f2baa0098bc2b0d3
A-c0rN <mayer.brigan@gmail.com>
2022-07-28 07:35:36 -0500

    Did the formatting shuffle  - Reformatted to PEP-8 standards (Except camelCase)  - Shuffled for possible efficiency  - Added BroadcasterMode  - Removed Class Attribs

--- EASGen/EASGen.py
+++ EASGen/EASGen.py
@@ -1,193 +1,209 @@
-
 from pydub import AudioSegment
 from pydub.generators import Sine
 
-class EASGen():
-    """Generate various EAS Alert Types.
-    """
-    
-    silence = AudioSegment.silent() ## 1 second silence, Used globally.
-    ATTNTones = AudioSegment.empty() ## Empty ATTN AudSegs for later population by the Init.
-    EOMs = AudioSegment.empty() ## Empty EOM AudSegs for later population by the Init.
 
+class EASGen:
+    """Generate various EAS Alert Types."""
 
-    def __init__(self, sampleRate:int=24000) -> None:
-
-        ## Normal ATTN - This consists of a 853 Hz tone x 960 Hz tone generated at BD 8 and sample of whatever for 8 sec. 
-        self.ATTNTones = self.__ATTN__(sampleRate)
-
-        ## Pregen EOMs
-        self.EOMs = self.__EOM__(sampleRate) ## Set the Global EOMs to the EOMs we just made, as 1 sec silence + EOM times 3
+    _silence = AudioSegment.silent()
 
     @classmethod
-    def __Mark__(cls, sampleRate:int=24000) -> AudioSegment:
-        ## Dynamic Samplerate MARK Sine generation (One Bit)
-        return Sine(2083.315, sample_rate=sampleRate, bit_depth=8).to_audio_segment(duration=float(1000/520.83), volume=-3)
+    def _mark(cls, sample_rate: int = 24000) -> AudioSegment:
+        return Sine(2083.3, sample_rate=sample_rate, bit_depth=16).to_audio_segment(
+            duration=float(1000 / 520.83), volume=-3
+        )
 
     @classmethod
-    def __Space__(cls, sampleRate:int=24000) -> AudioSegment:
-        ## Dynamic Samplerate SPACE Sine generation (One Bit)
-        return Sine(1562.485, sample_rate=sampleRate, bit_depth=8).to_audio_segment(duration=float(1000/520.83), volume=-3)
-    
+    def _space(cls, sample_rate: int = 24000) -> AudioSegment:
+        return Sine(1562.5, sample_rate=sample_rate, bit_depth=16).to_audio_segment(
+            duration=float(1000 / 520.83), volume=-3
+        )
+
     @classmethod
-    def __ATTN__(cls, mode:str="", sampleRate:int=24000):
-        ## Dynamic Samplerate Attention Tone Generation
+    def _attn(cls, mode: str = "", sample_rate: int = 24000):
         if mode == "NWS":
-            return Sine(1050, sample_rate=sampleRate, bit_depth=8).to_audio_segment(duration=9000, volume=-4)
+            return Sine(1050, sample_rate=sample_rate, bit_depth=16).to_audio_segment(
+                duration=9000, volume=-4
+            )
+        elif mode == "BROADCASTER":
+            return Sine(1050, sample_rate=44100, bit_depth=16).to_audio_segment(
+                duration=9000, volume=-4
+            )
         else:
-            return Sine(853, sample_rate=sampleRate, bit_depth=8).to_audio_segment(duration=8000, volume=-10).overlay(Sine(960, sample_rate=sampleRate, bit_depth=8).to_audio_segment(duration=8000, volume=-10))
+            return (
+                Sine(853, sample_rate=sample_rate, bit_depth=16)
+                .to_audio_segment(duration=8000, volume=-10)
+                .overlay(
+                    Sine(960, sample_rate=sample_rate, bit_depth=16).to_audio_segment(
+                        duration=8000, volume=-10
+                    )
+                )
+            )
 
     @classmethod
-    def __EOM__(cls, mode:str="", sampleRate:int=24000):
-        EOM = AudioSegment.empty() ## Pregen an AudioSegment object to populate with EOMs on the next lines
+    def _eom(cls, mode: str = "", sample_rate: int = 24000):
+        eom = AudioSegment.empty()
+        eoms = AudioSegment.empty()
         if mode == "NWS":
-            for bit in ''.join(format(ord(x), '08b')[::-1] for x in ('\xab'*16)+'NNNN'+('\x00'*2)): EOM += cls.__Space__(sampleRate) if bit == "0" else cls.__Mark__(sampleRate)
-            return (cls.silence + EOM)*3
+            for bit in "".join(
+                format(ord(x), "08b")[::-1]
+                for x in ("\xab" * 16) + "NNNN" + ("\x00" * 2)
+            ):
+                eom += cls._space(sample_rate) if bit == "0" else cls._mark(sample_rate)
+            eoms = (cls._silence + eom) * 3
         elif mode == "DIGITAL":
-            for bit in ''.join(format(ord(x), '08b')[::-1] for x in '\x00'+('\xab'*16)+'NNNN'+('\xff'*3)): EOM += cls.__Space__(sampleRate) if bit == "0" else cls.__Mark__(sampleRate)
-            data = cls.silence + EOM
-            EOM = AudioSegment.empty() ## Clear EOM value to make Juju stoof not happen.
-            for bit in ''.join(format(ord(x), '08b')[::-1] for x in ('\xab'*16)+'NNNN'+('\xff'*3)): EOM += cls.__Space__(sampleRate) if bit == "0" else cls.__Mark__(sampleRate)
-            data += (cls.silence + EOM)*2
-            return data
+            for bit in "".join(
+                format(ord(x), "08b")[::-1]
+                for x in "\x00" + ("\xab" * 16) + "NNNN" + ("\xff" * 3)
+            ):
+                eom += cls._space(sample_rate) if bit == "0" else cls._mark(sample_rate)
+            data = cls._silence + eom
+            eom = AudioSegment.empty()
+            for bit in "".join(
+                format(ord(x), "08b")[::-1]
+                for x in ("\xab" * 16) + "NNNN" + ("\xff" * 3)
+            ):
+                eom += cls._space(sample_rate) if bit == "0" else cls._mark(sample_rate)
+            data += (cls._silence + eom) * 2
+            eoms = data
         elif mode == "SAGE":
-            for bit in ''.join(format(ord(x), '08b')[::-1] for x in ('\xab'*16)+'NNNN\xff'): EOM += cls.__Space__(sampleRate) if bit == "0" else cls.__Mark__(sampleRate)
-            return (cls.silence + EOM)*3
+            for bit in "".join(
+                format(ord(x), "08b")[::-1] for x in ("\xab" * 16) + "NNNN\xff"
+            ):
+                eom += cls._space(sample_rate) if bit == "0" else cls._mark(sample_rate)
+            eoms = (cls._silence + eom) * 3
         elif mode == "TRILITHIC":
-            for bit in ''.join(format(ord(x), '08b')[::-1] for x in ('\xab'*16)+'NNNN'): EOM += cls.__Space__(sampleRate) if bit == "0" else cls.__Mark__(sampleRate)
-            return (cls.silence[:850] + EOM)*3
+            for bit in "".join(
+                format(ord(x), "08b")[::-1] for x in ("\xab" * 16) + "NNNN"
+            ):
+                eom += cls._space(sample_rate) if bit == "0" else cls._mark(sample_rate)
+            eoms = (cls._silence[:850] + eom) * 3
+        elif mode == "BROADCASTER":
+            for bit in "".join(
+                format(ord(x), "08b")[::-1]
+                for x in ("\xab" * 16) + "NNNN" + ("\x00" * 3)
+            ):
+                eom += cls._space(44100) if bit == "0" else cls._mark(44100)
+            eoms = (cls._silence + eom) * 3
         else:
-            for bit in ''.join(format(ord(x), '08b')[::-1] for x in ('\xab'*16)+'NNNN'): EOM += cls.__Space__(sampleRate) if bit == "0" else cls.__Mark__(sampleRate)
-            return (cls.silence + EOM)*3
-
+            for bit in "".join(
+                format(ord(x), "08b")[::-1] for x in ("\xab" * 16) + "NNNN"
+            ):
+                eom += cls._space(sample_rate) if bit == "0" else cls._mark(sample_rate)
+            eoms = (cls._silence + eom) * 3
+        return eoms
 
     @classmethod
-    def genEOM(cls, mode:str="", sampleRate:int=24000):
-        """
-        Generate SAME EOMs.(Inline Class, can be called without class Init)
-
-        :int sampleRate: 
-            Audio SampleRate
-
-        Returns a PyDub AudioSegment of the Generated SAME EOMs.
-        """
-        if cls.EOMs != AudioSegment.empty():
-            EOM = cls.EOMs
+    def _header(cls, header_data: str, mode: str = "", sample_rate: int = 24000):
+        header = AudioSegment.empty()
+        headers = AudioSegment.empty()
+        if mode == "NWS":
+            data = ("\xab" * 16) + header_data + ("\x00" * 2)
+            for bit in "".join(format(ord(x), "08b")[::-1] for x in data):
+                header += (
+                    cls._space(sample_rate) if bit == "0" else cls._mark(sample_rate)
+                )
+            headers = (header + cls._silence) * 3
+        elif mode == "DIGITAL":
+            data = "\x00" + ("\xab" * 16) + header_data + ("\xff" * 3)
+            for bit in "".join(format(ord(x), "08b")[::-1] for x in data):
+                header += (
+                    cls._space(sample_rate) if bit == "0" else cls._mark(sample_rate)
+                )
+            headers = header + cls._silence
+            data = ("\xab" * 16) + header_data + ("\xff" * 3)
+            header = AudioSegment.empty()
+            for bit in "".join(format(ord(x), "08b")[::-1] for x in data):
+                header += (
+                    cls._space(sample_rate) if bit == "0" else cls._mark(sample_rate)
+                )
+            headers += (header + cls._silence) * 2
+        elif mode == "SAGE":
+            data = ("\xab" * 16) + header_data + "\xff"
+            for bit in "".join(format(ord(x), "08b")[::-1] for x in data):
+                header += (
+                    cls._space(sample_rate) if bit == "0" else cls._mark(sample_rate)
+                )
+            headers = (header + cls._silence) * 3
+        elif mode == "TRILITHIC":
+            data = ("\xab" * 16) + header_data
+            for bit in "".join(format(ord(x), "08b")[::-1] for x in data):
+                header += (
+                    cls._space(sample_rate) if bit == "0" else cls._mark(sample_rate)
+                )
+            headers = (header + cls._silence[:850]) * 3 + cls._silence[:150]
+        elif mode == "BROADCASTER":
+            data = ("\xab" * 16) + header_data + ("\x00" * 3)
+            for bit in "".join(format(ord(x), "08b")[::-1] for x in data):
+                header += cls._space(44100) if bit == "0" else cls._mark(44100)
+            headers = (header + cls._silence) * 3
         else:
-            EOM = cls.__EOM__(mode=mode, sampleRate=sampleRate)
-        return EOM
+            data = ("\xab" * 16) + header_data
+            for bit in "".join(format(ord(x), "08b")[::-1] for x in data):
+                header += (
+                    cls._space(sample_rate) if bit == "0" else cls._mark(sample_rate)
+                )
+            headers = (header + cls._silence) * 3
+        return headers
 
     @classmethod
-    def genEAS(cls, header:str, attentionTone:bool=True, endOfMessage:bool=True, audio:AudioSegment=AudioSegment.empty(), mode:str="", sampleRate:int=24000) -> AudioSegment:
+    def genEOM(cls, mode: str = "", sampleRate: int = 24000):
+        """
+        Generate SAME _eoms.(Inline Class, can be called without class Init)
+        :int sample_rate:
+            Audio sample_rate
+        Returns a PyDub AudioSegment of the Generated SAME _eoms.
+        """
+        eom = cls._eom(mode=mode, sample_rate=sampleRate)
+        return eom
+
+    @classmethod
+    def genEAS(
+        cls,
+        header: str,
+        attentionTone: bool = True,
+        endOfMessage: bool = True,
+        audio: AudioSegment = AudioSegment.empty(),
+        mode: str = "",
+        sampleRate: int = 24000,
+    ) -> AudioSegment:
         """
         Generate EAS SAME from a String. (Inline Class, can be called without class Init)
-
-        :str hdr: 
+        :str hdr:
             Data to be modulated into SAME EAS Data.
-
-        :bool attentionTone: 
+        :bool attentionTone:
             Enable Internal Attention Tone (EBS Style 853+960).
-
-        :bool endOfMessage: 
+        :bool endOfMessage:
             Enable SAME End Of Message bursts.
-
-        :AudioSegment audio: 
-            Included audio in-between the SAME Header Bursts + Attention Tone, and the End Of Message Bursts.
-
-        :str mode: 
-            Select between "None, SAGE, DIGITAL, TRILITHIC, NWS" for "Authentic" Encoder header tones.
-
-        :int sampleRate: 
-            Audio SampleRate
-
+        :AudioSegment audio:
+            Included audio in-between the SAME header Bursts + Attention Tone, and the End Of Message Bursts.
+        :str mode:
+            Select between "None, SAGE, DIGITAL, TRILITHIC, BROADCASTER, NWS" for "Authentic" Encoder header tones.
+        :int sampleRate:
+            Audio sample rate
         Returns a PyDub AudioSegment of the Generated Alert.
         """
-
-        ## Create local variables with Empty AudioSegments for later population if required.
-        Header = AudioSegment.empty() 
-        ATTNTone = AudioSegment.empty()
-        EOMs = AudioSegment.empty()
-        
-        ##Generate EAS Data
-        if mode == "NWS":
-            data = ('\xab'*16)+header+('\x00'*2) ## Adding 16 bytes of Hex AB (Preamble) to the Headers (And 2 byes of Hex 00 to end of headers for "NWS" style)
-            for bit in ''.join(format(ord(x), '08b')[::-1] for x in data): ## Recursive loop to go through each byte of our data in LSB
-                Header += cls.__Space__(sampleRate) if bit == "0" else cls.__Mark__(sampleRate) ## If bit is a 0, append a Space to the Header Variable, else append a Mark
-            Headers = (Header + cls.silence)*3 ##Generate 3 header bursts by multiplying Headers + 1 second of silence by 3
-        elif mode == "DIGITAL":
-            data = '\x00'+('\xab'*16)+header+('\xff'*3) ## Adding 16 bytes of Hex AB (Preamble) to the Headers (And 3 byes of Hex FF to end of headers for "SAGE DIGITAL style" headers)
-            for bit in ''.join(format(ord(x), '08b')[::-1] for x in data): ## Recursive loop to go through each byte of our data in LSB
-                Header += cls.__Space__(sampleRate) if bit == "0" else cls.__Mark__(sampleRate) ## If bit is a 0, append a Space to the Header Variable, else append a Mark
-            Headers = Header + cls.silence ##Generate a header burst of the first header with the chirp. (SAGE Digital is Annoying AF)
-            data = ('\xab'*16)+header+('\xff'*3) ## Adding 16 bytes of Hex AB (Preamble) to the Headers (And 3 byes of Hex FF to end of headers for "SAGE DIGITAL style" headers)
-            Header = AudioSegment.empty() ## Clear header value to make Juju stoof not happen.
-            for bit in ''.join(format(ord(x), '08b')[::-1] for x in data): ## Recursive loop to go through each byte of our data in LSB
-                Header += cls.__Space__(sampleRate) if bit == "0" else cls.__Mark__(sampleRate) ## If bit is a 0, append a Space to the Header Variable, else append a Mark
-            Headers += (Header + cls.silence)*2 ##Generate 2 header bursts of the last 2 headers without the chirp. (SAGE Digital is REALLY Annoying AF)
-        elif mode == "SAGE":
-            data = ('\xab'*16)+header+'\xff' ## Adding 16 bytes of Hex AB (Preamble) to the Headers (And 1 byte of Hex FF to end of headers for "SAGE EAS style" headers)
-            for bit in ''.join(format(ord(x), '08b')[::-1] for x in data): ## Recursive loop to go through each byte of our data in LSB
-                Header += cls.__Space__(sampleRate) if bit == "0" else cls.__Mark__(sampleRate) ## If bit is a 0, append a Space to the Header Variable, else append a Mark
-            Headers = (Header + cls.silence)*3 ##Generate 3 header bursts by multiplying Headers + 1 second of silence by 3
-        elif mode == "TRILITHIC":
-            data = ('\xab'*16)+header ## Adding 16 bytes of Hex AB (Preamble) to the Headers (And 2 byes of Hex 00 to end of headers for "NWS" style)
-            for bit in ''.join(format(ord(x), '08b')[::-1] for x in data): ## Recursive loop to go through each byte of our data in LSB
-                Header += cls.__Space__(sampleRate) if bit == "0" else cls.__Mark__(sampleRate) ## If bit is a 0, append a Space to the Header Variable, else append a Mark
-            Headers = (Header + cls.silence[:850])*3+cls.silence[:150] ##Generate 3 header bursts by multiplying Headers + 0.85 second of silence by 3 (Smaller silence for "Trilithic style")
-        else:
-            data = ('\xab'*16)+header## Adding 16 bytes of Hex AB (Preamble) to the Headers
-            for bit in ''.join(format(ord(x), '08b')[::-1] for x in data): ## Recursive loop to go through each byte of our data in LSB
-                Header += cls.__Space__(sampleRate) if bit == "0" else cls.__Mark__(sampleRate) ## If bit is a 0, append a Space to the Header Variable, else append a Mark
-            Headers = (Header + cls.silence)*3 ##Generate 3 header bursts by multiplying Headers + 1 second of silence by 3
-
-        ## Set Audio to given audio with proper samplerate, channels, and width if provided.
+        sample_rate = sampleRate
+        attn_tone = AudioSegment.empty()
+        _eoms = AudioSegment.empty()
+        headers = cls._header(header=header, mode=mode, sample_rate=sample_rate)
         if audio != AudioSegment.empty():
-            audio = audio.set_frame_rate(sampleRate).set_channels(1).set_sample_width(2)	
-
-        ## Add the ATTN tone if requested (Default value is Empty)
+            audio = (
+                audio.set_frame_rate(sample_rate).set_channels(1).set_sample_width(2)
+            )
         if attentionTone:
-            if cls.ATTNTones == AudioSegment.empty():
-                ATTNTone = (cls.__ATTN__(mode=mode, sampleRate=sampleRate) + cls.silence)
-            else:
-                ATTNTone = (cls.ATTNTones + cls.silence) 	
-        
-        ## Add the EOMs if requested (Default value is Empty)
+            attn_tone = cls._attn(mode=mode, sample_rate=sample_rate) + cls._silence
+
         if endOfMessage:
-            EOMs = cls.genEOM(mode=mode, sampleRate=sampleRate)
-
-        ALERT = cls.silence[:500] + Headers + ATTNTone + audio + EOMs + cls.silence[:500] ## Alert adds 500MS of silence at beginning/end to allow multiple EAS tones played back-to-back to be properly audible.
-        return ALERT
-
-    def generateEOMAudio(self, sampleRate:int=24000):
-        """
-        Generate SAME EOMs.
-
-        Returns a PyDub AudioSegment of the Generated SAME EOMs.
-        Please note, Custom Style Headers are not supported with this class.
-        """
-        return self.genEOM(sampleRate=sampleRate)
-
-    def generateEASAudio(self, header:str, attentionTone:bool=True, endOfMessage:bool=True, audio:AudioSegment=AudioSegment.empty(), sampleRate:int=24000) -> AudioSegment:
-        """
-        Generate EAS SAME from a String.
-
-        :str hdr: 
-            Data to be modulated into SAME EAS Data.
-
-        :bool attentionTone: 
-            Enable Internal Attention Tone (EBS Style 853+960).
-
-        :bool endOfMessage: 
-            Enable SAME End Of Message bursts.
-
-        :AudioSegment audio: 
-            Included audio in-between the SAME Header Bursts + Attention Tone, and the End Of Message Bursts.
-
-        :int sampleRate: 
-            Audio SampleRate
-
-        Returns a PyDub AudioSegment of the Generated Alert.
-        Please note, Custom Style Headers are not supported with this class.
-        """
-        ALERT = self.genEAS(header=header, attentionTone=attentionTone, endOfMessage=endOfMessage, audio=audio, sampleRate=sampleRate)
-        return ALERT
+            _eoms = cls.genEOM(mode=mode, sample_rate=sample_rate)
+        alert: AudioSegment = (
+            cls._silence[:500]
+            + headers
+            + attn_tone
+            + audio
+            + _eoms
+            + cls._silence[:500]
+        )
+        if mode == "BROADCASTER":
+            alert.set_frame_rate(8000)
+        return alert
--- setup.py
+++ setup.py
@@ -1,39 +1,38 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='EASGen',
-    packages=['EASGen'],
-    version='0.1.6',
-    description='A Python library to generate EAS SAME Audio using Raw Data',
-    author='A-c0rN',
-    author_email='acrn@gwes-eas.network',
-    license='ODbL-1.0',
-    install_requires=['EASGen'],
+    name="EASGen",
+    packages=["EASGen"],
+    version="0.1.7",
+    description="A Python library to generate EAS SAME Audio using Raw Data",
+    author="A-c0rN",
+    author_email="acrn@gwes-eas.network",
+    license="ODbL-1.0",
+    install_requires=["EASGen"],
     long_description=README,
-    long_description_content_type='text/markdown',
-    url='https://github.com/A-c0rN/EASGen',
-    keywords='audio sound eas alerting emergency-alert-system',
+    long_description_content_type="text/markdown",
+    url="https://github.com/A-c0rN/EASGen",
+    keywords="audio sound eas alerting emergency-alert-system",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: Other/Proprietary License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
+        "Development Status :: 5 - Production/Stable",
+        "License :: Other/Proprietary License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
         "Topic :: Multimedia :: Sound/Audio",
         "Topic :: Software Development :: Libraries",
-        'Topic :: Utilities',
-    ]
+        "Topic :: Utilities",
+    ],
 )
-
